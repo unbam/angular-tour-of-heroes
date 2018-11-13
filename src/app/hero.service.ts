a6861a344ae10e059fc77cad59a5ebf13910bb84
Masashi Takeshita <mtakeshita@collabo-sys.com>
2018-11-13 17:36:26 +0900

    Create Routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,8 +11,22 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
+  /**
+   * HEROESリストの取得
+   * @returns {Observable<Hero[]>}
+   */
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: feched heroes');
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  /**
+   * 対象Heroの取得
+   * @param {number} id ID
+   * @returns {Observable<Hero>}
+   */
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add('HeroService: fetched hero');
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,23 +8,13 @@ import { HeroService } from "../hero.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     // リスト取得
     this.getHeroes();
-  }
-
-  /**
-   * 選択イベント
-   * @param {Hero} hero
-   */
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   /**
